7afdf71c396283ef4df7b176ba04954ab6321ee9
PastelSnowChild <n.lahteinen1@gmail.com>
2025-11-25 17:19:38 +0200

    connect to database debugging

--- matsukirppis_server/db/connection.js
+++ matsukirppis_server/db/connection.js
@@ -8,19 +8,19 @@ const connection = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DB,
-    // connectionLimit: 5,
+    connectionLimit: 5,
     dateStrings: true
 });
 
 const connectToDatabase= async ()=> {
-    // try{
-    //     await connection.getConnection()
-    //     console.log("MySQL connection works!")
+    try{
+        await connection.getConnection()
+        console.log("MySQL connection works!")
 
-    // }catch(e){
-    //     console.log("Database error".e)
-    //     throw e;
-    // }
+    }catch(e){
+        console.log("Database error".e)
+        // throw e;
+    }
 }
 
 
