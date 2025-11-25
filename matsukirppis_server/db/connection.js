d772f980ea88600605d6bf4656e3bdd955e42bc9
PastelSnowChild <n.lahteinen1@gmail.com>
2025-11-25 16:57:14 +0200

    changed connection limit

--- matsukirppis_server/db/connection.js
+++ matsukirppis_server/db/connection.js
@@ -8,7 +8,7 @@ const connection = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DB,
-    connectionLimit: 5,
+    // connectionLimit: 5,
     dateStrings: true
 });
 
